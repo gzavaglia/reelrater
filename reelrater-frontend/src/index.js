c4905b233af5bfa971e1d01728c6a98221e122cd
gzavaglia <gio.zavaglia@gmail.com>
2020-03-01 12:33:51 -0600

    added movies reducer and installed dependencies

--- reelrater-frontend/src/index.js
+++ reelrater-frontend/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import movieReducer from './reducers/movieReducer'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(movieReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(
+<Provider>
+    <App />
+</Provider>, 
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
